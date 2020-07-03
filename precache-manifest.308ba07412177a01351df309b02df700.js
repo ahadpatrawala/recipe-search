b40fb113754d0aa4275bc58b3334a495de45e750
Alahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2020-07-02 18:07:19 -0700

    Updates

---
+++ precache-manifest.308ba07412177a01351df309b02df700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5e146511ac259a83d9bb9fd890775c",
+    "url": "/recipe-search/index.html"
+  },
+  {
+    "revision": "17cab5430fc6134cc44f",
+    "url": "/recipe-search/static/css/main.e66ecd5d.chunk.css"
+  },
+  {
+    "revision": "0259ee1baeac2ab16294",
+    "url": "/recipe-search/static/js/2.301320e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-search/static/js/2.301320e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17cab5430fc6134cc44f",
+    "url": "/recipe-search/static/js/main.279887f6.chunk.js"
+  },
+  {
+    "revision": "adaaa64425bef03b5861",
+    "url": "/recipe-search/static/js/runtime-main.be763e63.js"
+  }
+]);
